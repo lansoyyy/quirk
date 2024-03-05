5102406b923f62103f5dcb055d0a112dbd70b71c
Lans12345 <olanalans12345@gmail.com>
2024-03-06 07:26:30 +0800

    added initial login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+import 'package:quirk/utlis/colors.dart';
+import 'package:quirk/widgets/button_widget.dart';
+import 'package:quirk/widgets/textfield_widget.dart';
+
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+
+  final passwordController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: background,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              // Sample logo
+              Image.asset(
+                'assets/images/logo.png',
+                height: 200,
+              ),
+              const SizedBox(
+                height: 75,
+              ),
+              TextFieldWidget(
+                borderColor: primary,
+                label: 'Email',
+                controller: emailController,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFieldWidget(
+                isObscure: true,
+                borderColor: primary,
+                label: 'Password',
+                controller: passwordController,
+                showEye: true,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ButtonWidget(
+                radius: 10,
+                color: primary,
+                width: 300,
+                label: 'Login',
+                onPressed: () {
+                  // Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  //     builder: (context) => const HomeScreen()));
+                },
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
